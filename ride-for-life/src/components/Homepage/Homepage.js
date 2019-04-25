c6e5cc4c2652a0cddcc609606d2aa69b4b1b72e6
Claire Sinozich <claire.sinozich@gmail.com>
2019-04-24 21:29:52 -0600

    homepage start

---
+++ ride-for-life/src/components/Homepage/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="wrapper">
+      <div className="header-buttons">
+        <a href="#">Log In</a>
+        <a href="#">Sign Up</a>
+      </div>
+      <div className="main-text">Welcome to the New Standard of Care.</div>
+      <div className="main-buttons">
+        <a href="#">Learn More</a>
+        <a href="#">Sign Up</a>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage

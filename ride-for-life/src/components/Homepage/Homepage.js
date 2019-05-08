5543364afa5abbcbd64ead30a3085801f5308541
Claire Sinozich <claire.sinozich@gmail.com>
2019-05-08 12:37:33 -0600

    Homepage refactored

--- ride-for-life/src/components/Homepage/Homepage.js
+++ ride-for-life/src/components/Homepage/Homepage.js
@@ -2,41 +2,23 @@ import React, { Component } from "react";
 import { Route, Link } from 'react-router-dom';
 
 import { Container, Button } from '../../simple-library';
-import { MobileContainer, MobileButton } from '../../simple-library-mobile';
 
 class Homepage extends Component {
   state = {
     signUpHover: false,
-    logInHover: false, 
+    logInHover: false,
     signUpClick: false,
     logInClick: false
   }
 
-  container = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-
-  signUpTop = {
-    height: '50px',
-    width: '150px',
-    fontSize: '15px',
-    margin: '10px'
-  }
-
-  openForm = form => {
-    this.props.history.push(`signup/${form}`)
-  }
-
+//Functions
   componentDidMount() {
     window.addEventListener('click', () => this.windowClick())
     this.windowClick()
   }
 
-  windowClick = () => {
-    this.setState({ logInclick: false, signUpClick: false })
+  openForm = form => {
+    this.props.history.push(`signup/${form}`)
   }
 
   openLogin = event => {
@@ -49,9 +31,42 @@ class Homepage extends Component {
     this.setState({ signUpClick: true })
   }
 
-  topButton = {
-    width: '70vw',
-    margin: '10px auto'  
+  windowClick = () => {
+    this.setState({ logInclick: false, signUpClick: false })
+  }
+
+//Styles
+  buttons = mobile => ({
+    width: '100%',
+    display: 'flex',
+    flexDirection: this.props.mobile ? 'column' : 'row',
+    alignItems: this.props.mobile ? 'center' : null
+  })
+
+  container = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+  header = mobile => ({
+    fontSize: '4rem',
+    color: 'white',
+    textAlign: 'center',
+    width: this.props.mobile ? '90%' : '100%',
+    margin: this.props.mobile ? '200px 0 0' : null
+  })
+
+  mainBtn = {
+    background: 'none',
+    border: '2px solid #fff',
+    width: '250px',
+    height: '60px',
+    fontSize: '17px',
+    fontWeight: '600',
+    margin: '30px',
+    cursor: 'pointer'
   }
 
   openLoginTop = {
@@ -64,229 +79,102 @@ class Homepage extends Component {
     margin: '10px'
   }
 
-  mobileHeader = {
-    width: '90%',
-    margin: '200px 0 0'
+  signUpHover = {
+    display: 'flex',
+    flexDirection: 'column'
   }
 
-  mobileButtons = {
-    width: '100%',
+  signUpTop = {
+    height: '50px',
+    width: '150px',
+    fontSize: '15px',
+    margin: '10px'
+  }
+
+  top = mobile => ({
+    position: 'absolute',
+    top: '0',
+    right: '0',
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
+    flexDirection: this.props.mobile ? 'column' : null,
+    alignItems: this.props.mobile ? 'center' : null,
+    width: '100%'
+  })
+
+  topButton = {
+    width: '70vw',
+    margin: '10px auto'
+  }
+
+  topBtn = {
+    backgroundColor: 'rgba(225,225,225,0.7)',
+    color: '#4f6d7a',
+    border: '1px solid white',
+    width: '150px',
+    height: '50px',
+    margin: '10px',
+    fontSize: '15px',
+    textAlign: 'center'
   }
 
   render() {
-    // there are multiple ways to use this method. you can either do an if statement in render to render a completely different page, or you can write a function to conditionally load styles, and pass the boolean from props as a parameter, then use ternary operators to do conditional styling based on whether it's mobile or not
-    if(this.props.mobile) {
       return (
-        <MobileContainer 
-          home
-          img = { require('../../assets/views/home-mobile.jpeg')} 
-          style = {this.container}>
-            <MobileTop>
-              <div onClick = { event => this.openLogin(event) }>
-                { this.state.logInclick ? 
-                  <>
-                    <MobileButton style={this.openLoginTop}>
-                      Rider
-                    </MobileButton>
-                    <MobileButton style={this.openLoginTop}>
-                      Mother
-                    </MobileButton>
-                  </>
-                : 
-                  <Link to='/login'>
-                    <MobileButton style={this.topButton}>
-                      Log In
-                    </MobileButton>
-                  </Link>
-                }
-              </div>
-              <div onClick = { event => this.openSignup(event) }>
-                { this.state.signUpClick ? 
-                  <>
-                    <Link to='/signup/driver'><MobileButton style={this.openSignupTop}>Driver</MobileButton></Link>
-                    <Link to='/signup/mother'><MobileButton style={this.openSignupTop}>Mother</MobileButton></Link>
-                    <Link to='/signup/caregiver'><MobileButton style={this.openSignupTop}>Caregiver</MobileButton></Link>
-                  </>
-                : 
-                  <MobileButton style={this.topButton}>
-                    Sign Up
-                  </MobileButton>
-                }
-              </div>
-          </MobileTop>
-          <div  style={this.mobileHeader}><Header>Welcome to the New Standard of Care</Header></div>
-          <div style={this.mobileButtons}>
-            <Link to='/about'>
-              <MobileButton
-                style = { mainBtn }>
-                Learn More
-              </MobileButton></Link>
-              <Link to = "/signup">
-                <MobileButton
-                  style = { mainBtn }
-                >
-                  Sign Up
-                </MobileButton>
-              </Link>
-          </div>
-        </MobileContainer>
-      )
-    } 
-    else {
-      return (
-        <Container 
+        <Container
           home
           img = { require('../../assets/views/home.jpeg') }
-          style = { this.container }
-        >
-          <Top>
+          style = { this.container } >
+          <div style={ this.top(this.props.mobile) }>
             <Link to="/login">
               <Button
-                style = { topBtn }
-              >
+                style = { this.topBtn }>
                 Log In
               </Button>
             </Link>
             <div
               onMouseEnter = { this.state.signUpHover ? null : () => this.setState({ signUpHover: true }) }
-              onMouseLeave = { this.state.signUpHover ? () => this.setState({ signUpHover: false }) : null }
-            >
-              { this.state.signUpHover ? 
-                <div style = {{
-                  display: 'flex',
-                  flexDirection: 'column'
-                }}>
-                <Button 
-                  style = { topBtn }
-                  onClick = { () => this.openForm('driver') }
-                >
-                  Driver
-                </Button>
-                <Button 
-                  style = { topBtn }
-                  onClick = { () => this.openForm('mother') }
-                >
-                  Mother
-                </Button>
-                <Button 
-                  style = { topBtn }
-                  onClick = { () => this.openForm('caregiver') }
-                >
-                  Caregiver
-                </Button>
+              onMouseLeave = { this.state.signUpHover ? () => this.setState({ signUpHover: false }) : null } >
+              { this.state.signUpHover
+                ? <div style = { this.signUpHover }>
+                  <Button
+                    style = { this.topBtn }
+                    onClick = { () => this.openForm('driver') }>
+                    Driver
+                  </Button>
+                  <Button
+                    style = { this.topBtn }
+                    onClick = { () => this.openForm('mother') }>
+                    Mother
+                  </Button>
+                  <Button
+                    style = { this.topBtn }
+                    onClick = { () => this.openForm('caregiver') }>
+                    Caregiver
+                  </Button>
                 </div>
-              : 
-                <Button style={ this.signUpTop }>Sign Up</Button>
+              : <Button style={ this.signUpTop }>Sign Up</Button>
               }
             </div>
-          </Top>
-          <div>
-            <Header>Welcome to the New Standard of Care</Header>
           </div>
-          <MainButtons>
+          <div>
+            <h1 style={ this.header(this.props.mobile) }>Welcome to the New Standard of Care</h1>
+          </div>
+          <div style={ this.buttons(this.props.mobile) }>
             <Link to='/about'>
               <Button
-                style = { mainBtn }
-              >
+                style = { this.mainBtn }>
                 Learn More
               </Button>
             </Link>
             <Link to = "/signup">
               <Button
-                style = { mainBtn }
-              >
+                style = { this.mainBtn }>
                 Sign Up
               </Button>
             </Link>
-          </MainButtons>
+          </div>
         </Container>
       );
     }
   };
-}
-
-const Top = props => {
-  const style = {
-    position: 'absolute',
-    top: '0',
-    right: '0',
-    display: 'flex'
-  }
-
-  return (
-    <div style = { style }>
-      { props.children }
-    </div>
-  );
-}
-
-const MobileTop = props => {
-  const style = {
-    position: 'absolute',
-    top: '0',
-    right: '0',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '100%'
-  }
-
-  return (
-    <div style= {style}>
-      { props.children }
-    </div>
-  )
-}
-
-const Header = props => {
-  const style = {
-    fontSize: '4rem',
-    color: 'white',
-    textAlign: 'center'
-  }
-
-  return (
-    <h1 style = { style }>
-      { props.children }
-    </h1>
-  )
-}
-
-const topBtn = {
-  backgroundColor: 'rgba(225,225,225,0.7)',
-  color: '#4f6d7a',
-  border: '1px solid white',
-  width: '150px',
-  height: '50px',
-  margin: '10px', 
-  fontSize: '15px',
-  textAlign: 'center'
-}
-
-const MainButtons = props => {
-  const style = {
-
-  }
-
-  return (
-    <div style = { style }>
-      { props.children }
-    </div>
-  )
-}
-
-const mainBtn = {
-  background: 'none',
-  border: '2px solid #fff',
-  width: '250px',
-  height: '60px',
-  fontSize: '17px',
-  fontWeight: '600',
-  margin: '30px',
-  cursor: 'pointer'
-}
 
 export default Homepage;
